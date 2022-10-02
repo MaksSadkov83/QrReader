a730d6116adb471a74e9c4224b87b9eff31b548d
MaksSadkov83 <maxraptor432@gmail.com>
2022-10-02 22:20:09 +0300

    Add horizontal Navigation Menu

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from kivy.lang import Builder
 
 from kivymd.app import MDApp
+from kivymd.uix.tab import MDTabsBase
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.icon_definitions import md_icons
 
+
+class Tab(MDFloatLayout, MDTabsBase):
+    pass
 class Example(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
         return Builder.load_file("app_style_main.kv")
+
+    def on_start(self):
+        list_tabs = {
+            "camera": "Камера",
+            "magnify": "Реузльтат сканирования",
+            "chat-question": "Справка",
+        }
+        for icon_tab, name_tab in list_tabs.items():
+            self.root.ids.tabs.add_widget(Tab(icon=icon_tab, title=name_tab))
+
 
 
 Example().run()
