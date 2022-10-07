27da67c8bc60cdacd4058cb1ebe64c70b6b68151
MaksSadkov83 <maxraptor432@gmail.com>
2022-10-07 21:38:22 +0300

    Add Grid Bottom Sheet, update Camera, add content in Reference

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.tab import MDTabsBase
 from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.icon_definitions import md_icons
+
+from kivymd.toast import toast
+from kivymd.uix.bottomsheet import MDGridBottomSheet
 import time
+from kivymd.uix.label import MDLabel
 
 class Tab(MDFloatLayout, MDTabsBase):
     pass
@@ -13,8 +16,6 @@ class QRReaderApp(MDApp):
         self.theme_cls.theme_style = "Dark"
         return Builder.load_file("QRReader.kv")
 
-    def on_start(self):
-        pass
     def on_tab_switch(
         self, instance_tabs, instance_tab, instance_tab_label, tab_text
     ):
@@ -28,20 +29,46 @@ class QRReaderApp(MDApp):
         '''
 
         count_icon = instance_tab.icon  # get the tab icon
-        if count_icon == 'camera':
-           pass
-        elif count_icon == 'magnify':
-            pass
-        else:
-            pass
+
+
     def capture(self):
+        '''
+        Function to capture the images and give them the names
+        according to their captured time and date.
+        '''
         camera = self.root.ids['camera']
         timestr = time.strftime("%Y%m%d_%H%M%S")
         camera.export_to_png("IMG_{}.png".format(timestr))
-        print("Есть фотка")
 
+    def reader_qr(self):
+        pass
 
+    def github(self):
+        pass
 
+    def style_change(self):
+        pass
+
+    def callback_for_menu_items(self, *args):
+        toast(args[0])
+
+    def show_grid_bottom_sheet(self):
+        bottom_sheet_menu = MDGridBottomSheet()
+        data = {
+            "WhatsApp": "whatsapp",
+            "VK": "chat",
+            "Telegram": "navigation-variant"
+        }
+        for item in data.items():
+            bottom_sheet_menu.add_item(
+                item[0],
+                lambda x, y=item[0]: self.callback_for_menu_items(y),
+                icon_src=item[1],
+            )
+        bottom_sheet_menu.open()
+
+    def write_to_developer(self):
+        pass
 
 if __name__ == "__main__":
     QRReaderApp().run()
